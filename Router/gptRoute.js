12930616d33cc63bfe72968900f02e0adf6e49c1
unknown <vuongchanvinh20@gmail.com>
2023-05-28 00:04:55 +1000

    First commit

--- Router/gptRoute.js
+++ Router/gptRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-require("dotenv").config();
+
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
